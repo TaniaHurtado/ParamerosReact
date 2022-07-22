00e5582bc674ddaaa50e8ad04c2eb0a55d6f759b
unknown <tphurtadop@unal.ecu.co>
2022-07-21 21:34:33 -0500

    Add map

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,13 +7,6 @@ const Footer = () => {
         <div className='footer-info'>
             <h1> Parameros </h1>
             <p> Mongua-Boyacá</p>
-        </div>
-        <div className='footer-contact'>
-            <h3> Contactanos</h3>
-            <span> Y ven a vivirlo con nosotros</span>
-        </div>
-        <div className='footer-sns'>
-            <div> By Parameros</div>
             <div className='sns-links'>
                 <a href="https://www.facebook.com/profile.php?id=100082044590331" target='_blank' rel="noopener noreferrer" >
                     <i className='fab fa-facebook facebook'></i>
@@ -24,7 +17,9 @@ const Footer = () => {
 
             </div>
         </div>
-
+        <div className='footer-contact'>
+        <iframe src="https://widgets.scribblemaps.com/sm/?d=true&z=true&l=true&id=09qiLUUzpb&s" allow="geolocation" allowfullscreen width="550" height="550" frameborder="0" loading="lazy" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        </div>
 
     </footer>
   )
--- src/components/slider/Slides.js
+++ src/components/slider/Slides.js
@@ -16,7 +16,9 @@ const slidesInfo = [
         title: 'Caminata ecológica Laguna Negra',
         desc: 'Ven a conocer la laguna negra. Encontraremos dos lindas cascadas que alimentan la laguna, y podrás apreciar las montañas del páramo de Ocetá, considerado el mas lindo del mundo',
         cost: 'Desde 80.000COP',
-        includes: ['Transporte local', 'Almuerzo viajero tipo sandwich', 'guía local', 'entrada a la laguna']
+        includes: ['Transporte local', 'Almuerzo viajero tipo sandwich', 'guía local', 'entrada a la laguna'],
+        time: '5 Horas',
+        distance: '5 Km'
 
     },
     {
@@ -25,7 +27,9 @@ const slidesInfo = [
         title: 'Caminata ecológica Lagunas de la Estrella',
         desc: 'Caminata con recorridos por quebradas y visita de dos lagunas en el páramo de San Ignacio, avistamiento de aves y flora típica del páramo, recorrido por camino real hacia los llanos orientales',
         cost: 'Desde 100.000COP',
-        includes: ['Transporte local', 'Almuerzo viajero tipo sandwich', 'guía local', 'Visita dos lagunas', 'Visita páramo San Ignacio']
+        includes: ['Transporte local', 'Almuerzo viajero tipo sandwich', 'guía local', 'Visita dos lagunas', 'Visita páramo San Ignacio'],
+        time: '7 Horas',
+        distance: '7 Km'
     },
     {
         src: hospedaje,
@@ -33,7 +37,9 @@ const slidesInfo = [
         title: 'Hospedaje rural',
         desc: 'Hospedaje en tradicional finca campesina en donde podrás participar de las actividades diarias del campo, disfrutar de una fogata y de la mejor comida hecha en estufa de carbón',
         cost: 'Desde 60.000COP',
-        includes: ['Desayuno campesino', 'Recorrido por la finca', 'Fogata', 'hospedaje x noche']
+        includes: ['Desayuno campesino', 'Recorrido por la finca', 'Fogata', 'hospedaje x 1 noche'],
+        time: '1 noche',
+        distance: '1 Km'
     },
     {
         src: siembras,
@@ -41,7 +47,9 @@ const slidesInfo = [
         title: 'Caminata y Siembra de árboles',
         desc: 'Siembra de frailejones y/o árboles nativos en el páramo Los Higuerones aportando a la recuperación de los terrenos antes utilizados en ganadería.',
         cost: 'Desde 80.000COP',
-        includes: ['Frailejón', 'Almuerzo tipo sandwich', 'Caminata con guía local', 'Siembra']
+        includes: ['Frailejón', 'Almuerzo tipo sandwich', 'Caminata con guía local', 'Siembra'],
+        time: '5 Horas',
+        distance: '4.5 Km'
     },
     {
         src: caminataRio,
@@ -49,7 +57,9 @@ const slidesInfo = [
         title: 'Caminata y baño de río',
         desc: 'Participa de esta caminata por Las Piedras y antiguo molino, disfruta de un refrescante y medicinal baño de río en las heladas aguas del rio saidube',
         cost: 'Desde 30.000COP',
-        includes: ['Caminata al rio', 'Caminata mirador La Peña', 'Guía local', 'Fotografías']
+        includes: ['Caminata al rio', 'Caminata mirador La Peña', 'Guía local', 'Fotografías'],
+        time: '3 Horas',
+        distance: '1.5 Km'
     }
 ]
 
@@ -84,6 +94,9 @@ const slides = slidesInfo.map(slide => (
                         })
                     }
 
+                </div>
+                <div className='slide-button'>
+                <a href="https://wa.me/message/COZWMJFFXZGHF1"  className='button medium-button'>RESERVAR</a>
                 </div>
             </div>
         </div>
